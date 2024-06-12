9fee09ff3842fe552c69a2d0c4d94e3eda0066c2
Ankit Yadav <118040049+Code-o-pathy@users.noreply.github.com>
2024-06-12 08:05:16 +0000

    memory in js

---
+++ basic/memory.js
@@ -0,0 +1,7 @@
+// in prior languages memory allocations and deallocations should have been done by programmer himself, but thanks to moren languages , automatic allo and deallo are done, automatic garbage collections are done.
+
+//in js there are two types of memory 
+//stack and heap.
+//stack in primitive datatypes(call by value,gets copy of the variable changes made wont affect the original copy o the variable)
+//head in non primitive datatypes(call by reference ,gets reference i.e actual location of reference and make changes to original copy of variable)
+
